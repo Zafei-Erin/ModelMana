a459d6f7a66c01beca2cdccd0f421c532cc5adf4
Zafei-Erin <897659752@qq.com>
2026-01-02 18:18:52 +0800

    Initial Commit

---
+++ ModelMana/ModelManaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModelManaApp.swift
+//  ModelMana
+//
+//  Created by Zafei on 2/1/26.
+//
+
+import SwiftUI
+
+@main
+struct ModelManaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ModelManaTests/ModelManaTests.swift
@@ -0,0 +1,17 @@
+//
+//  ModelManaTests.swift
+//  ModelManaTests
+//
+//  Created by Zafei on 2/1/26.
+//
+
+import Testing
+@testable import ModelMana
+
+struct ModelManaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
